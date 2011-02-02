3f0cc0b7910990ca40a1f542c42fbfdaf74f5dd0
Bernardo Heynemann <heynemann@gmail.com>
2011-02-02 10:55:27 -0200

    Updates

--- src/whoami.core.js
+++ src/whoami.core.js
@@ -1,77 +1,69 @@
 // Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 
 var whoami = {
-    platform: null,
-    browser: null,
+
+    userAgent: navigator.userAgent.toLowerCase(),
+    browserPlatform: navigator.platform.toLowerCase(),
+
+    platform: {
+        name: ''
+    },
+
+    browser: {
+        name: ''
+    },
+
+    modules: {
+        platform: [],
+        browser: []
+    },
 
     type: {
-        platform: "platform",
-        browser: "browser"
-    },
-
-    init: function() {
-        this.platform = null;
-        this.browser = null;
-        this.clearModules();
-    },
-
-    clearModules: function() {
-        this.modules = {
-            platform: [],
-            browser: []
-        }
+        platform: 'platform',
+        browser: 'browser'
     },
 
     register: function(type, module) {
         this.modules[type].push(module);
     },
 
+    detect: function(collection, property) {
+        for (var i=0; i<collection.length; i++){
+            collection[i].call(this);
+        }
+    },
+
     identify: function(){
-        var context = {
-            context: null,
-            platform: null,
-            userAgent: navigator.userAgent.toLowerCase(),
-            browserPlatform: navigator.platform.toLowerCase()
-        };
+        this.detect(this.modules.platform, 'platform');
+        this.detect(this.modules.browser, 'browser');
+    },
 
-        var detect = function(collection, context, property) {
-            for (var i=0; i<collection.length; i++){
-                var module = collection[i];
-                var detected = module(context);
-
-                if (context[property]) {
-                    break;
-                }
-            }
-        };
-
-        detect(this.modules.platform, context, 'platform');
-        detect(this.modules.browser, context, 'browser');
-
-        this.browser = context.browser || null;
-        this.platform = context.platform || null;
+    setPlatformName: function(name) {
+        this.platform.name = name;
+        this.platform[name] = true;
     },
 
     helpers: {
         platform: {
-            identifyFromPlatform: function(regex, name) {
-                var pattern = regex;
-                return function(context) {
-                    if (pattern.test(context.browserPlatform)) {
-                        context.platform = name;
+            identifyFromPlatform: function(pattern, name) {
+                var self = whoami;
+
+                return function() {
+                    if (pattern.test(self.browserPlatform)) {
+                        self.setPlatformName(name);
                     }
                 };
             },
-            identifyFromUserAgent: function(regex, name) {
-                var pattern = regex;
-                return function(context) {
-                    if (pattern.test(context.userAgent)) {
-                        context.platform = name;
+            identifyFromUserAgent: function(pattern, name) {
+                var self = whoami;
+
+                return function() {
+                    if (pattern.test(self.userAgent)) {
+                        self.setPlatformName(name);
                     }
                 };
             }
         }
     }
-};
 
-whoami.init();
+};
